5bb4c370139d9d53a0d631fd946a059b2980a118
Juan <juanmanuelfernandez2@outlook.com>
2021-07-15 09:59:43 -0300

    imagenes dinamicas agregadas

--- src/App.js
+++ src/App.js
@@ -3,18 +3,27 @@ import Title from "./components/Title"
 import Card from "./components/Card"
 import Footer from "./components/Footer"
 
-const links = ["https://www.holoweb.net/~liam/pictures/2007-06-29-san-vivaldo/pages/img_4979/img_4979-300x200.jpg","https://diccionarioactual.com/wp-content/uploads/2017/01/l%C3%ADnea-300x200.jpg","https://diccionarioactual.com/wp-content/uploads/2016/10/foliaci%C3%B3n-300x200-2.jpg", "https://lh3.googleusercontent.com/proxy/NtPnYYj_yLh7opcPivreR7qUkfWieC2DxiK3xix3085fktCpwCM5q29j4Mo9rndvdnjbQ7VN3NvIvL1xZTTavSGkIZW3f3tpuLmUGhQ4CDZpiHBcD4paYLmLt7m2-Gflw3y5h3BX-aKgmSYPRss"];
+const links = ["https://www.holoweb.net/~liam/pictures/2007-06-29-san-vivaldo/pages/img_4979/img_4979-300x200.jpg","https://diccionarioactual.com/wp-content/uploads/2017/01/l%C3%ADnea-300x200.jpg","https://diccionarioactual.com/wp-content/uploads/2016/10/foliaci%C3%B3n-300x200-2.jpg","https://lh3.googleusercontent.com/proxy/ilcMOooM4ZpHxYkyKFTHnmnZ6xF0Q7HO9sSkftJU1e7VrKX5NzPabMX02FBEcnqQb8l7_BmW14o5ydDa38JM5BSepK3xhrtG1rIviZRkFAeJUijL0Grc6w553GrdUQtJLX4TreG1CZULrk9SLGE"];
 
+
+// React interpreta solo los arrays por eso está bueno pasarle arrays
+
+// const elementosHtml = [<h1>Hola</h1>,<h2>A</h2>,<i>as</i>]
 function App() {
   return (
     <div className="container-fluid vh-100">
       <Navbar/>
       <Title/>
       <div className="cards d-flex justify-content-around flex-wrap">
+        {/* Aca lo que hacemos es mapear el array (este metodo nos devuelve otro array), y usamos el parametro
+        en un prop del elemento JSX Card: */}
+        {links.map((url)=><Card url={url} />)}
+        {/* Este es el array de elementos html que directamente puede interpretar React */}
+        {/* {elementosHtml} */}
+        {/* <Card/>
         <Card/>
         <Card/>
-        <Card/>
-        <Card/>
+        <Card/> */}
       </div>
       <Footer/>
     </div>
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,9 @@
 // sfc = codigo para armar un componente
 
-const Card = () => {
+const Card = ({url}) => {
     return (
-        <div className="card  mt-3 mt-xl-0" style={{width: "18rem"}}>
-          <img src="https://www.holoweb.net/~liam/pictures/2007-06-29-san-vivaldo/pages/img_4979/img_4979-300x200.jpg" className="card-img-top" alt="..."/>
+        <div className="card  mt-3 mt-xl-0 mb-4" style={{width: "18rem"}}>
+          <img src={url} className="card-img-top" alt="..."/>
           <div className="card-body">
             <h5 className="card-title">Card title</h5>
             <p className="card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam excepturi omnis inventore qui eius iste ad non cum magnam id maiores tempore laborum illo velit distinctio, culpa soluta voluptas minus..</p>
